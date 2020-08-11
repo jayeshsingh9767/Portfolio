34a40b6a3f4e4c473bd3ea324e9f03d1eec8f937
Jayesh Singh <jayeshsingh9767@gmail.com>
2020-08-11 10:55:41 +0530

    changed db from mysql to sqlite3 due to server fault

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -76,21 +76,21 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'Portfolio/db.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': 'Portfolio/db.cnf',
+#         },
+#     }
+# }
 
 
 # Password validation
